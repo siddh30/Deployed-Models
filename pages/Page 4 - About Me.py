6d1848e88328793675c60a9bf659afc329a0eb96
siddh30 <siddh30@gmail.com>
2023-03-28 12:50:58 -0400

    Update

--- pages/Page 4 - About Me.py
+++ pages/Page 4 - About Me.py
@@ -2,43 +2,41 @@ import streamlit as st
 from streamlit_lottie import st_lottie
 from utils import load_lottieurl
 from bokeh.models.widgets import Div
+import urllib
 
 st.sidebar.image('images/me.png', width=300, caption="AI/ML Data Scientist")
 
 st.title("About Me")
 
-st.success('Data scientist applying principles of AI/ML & Data Science to help the firm tackle challenges, automate workflows and build profitable solutions in People Analytics.I love working on projects involving Natural Language Processing, Computer Vision, Recommender Systems, and Python. I am a Kaggle Expert and a novice blogger on Medium. I love gaming, dining, coding, playing soccer, and making & mixing music which I occasionally upload on SoundCloud.')
+st.success('My name is Siddharth Mandgi and I am a Data scientist applying principles of AI/ML & Data Science to help organizations tackle challenges, automate workflows and build profitable solutions in People Analytics.I love working on projects involving Natural Language Processing, Computer Vision, Recommender Systems, and Python. I am a Kaggle Expert and a novice blogger on Medium. I love gaming, dining, coding, playing soccer, and making & mixing music which I occasionally upload on SoundCloud.')
 
 
-with st.expander("My Socials!"):
+# with st.expander("My Socials!"):
 
-    col1, col2, col3 = st.columns(3)
+#     col1, col2, col3 = st.columns(3)
 
-    with col1:
-        url = "https://www.linkedin.com/in/siddharthmandgi/"
-        if st.button("Linkedin", use_container_width=True):
-                #js = "window.open" + "('" + url + "')"  # New tab
-                js = "window.location.href" + " = " + "'" + url + "'"  # Current tab
-                html = '<img src onerror="{}">'.format(js)
-                div = Div(text=html)
-                st.bokeh_chart(div)
+#     with col1:
+#         url = "https://www.linkedin.com/in/siddharthmandgi/"
+#         if st.button("Linkedin", use_container_width=True):
+#                 js = "window.open" + "('" + url + "')"  # New tab
+#                 html = '<img src onerror="{}">'.format(js)
+#                 div = Div(text=html)
+#                 st.bokeh_chart(div)
 
-    with col2:
-        url = "https://github.com/siddh30"
-        if st.button("Github", use_container_width=True):
-                #js = "window.open" + "('" + url + "')"  # New tab
-                js = "window.location.href" + " = " + "'" + url + "'"  # Current tab
-                html = '<img src onerror="{}">'.format(js)
-                div = Div(text=html)
-                st.bokeh_chart(div)
+#     with col2:
+#         url = "https://github.com/siddh30"
+#         if st.button("Github", use_container_width=True):
+#                 js = "window.open" + "('" + url + "')"  # New tab
+#                 html = '<img src onerror="{}">'.format(js)
+#                 div = Div(text=html)
+#                 st.bokeh_chart(div)
 
-    with col3:
-        url = "https://medium.com/@siddh30"
-        if st.button("Medium", use_container_width=True):
-                #js = "window.open" + "('" + url + "')"  # New tab
-                js = "window.location.href" + " = " + "'" + url + "'"  # Current tab
-                html = '<img src onerror="{}">'.format(js)
-                div = Div(text=html)
-                st.bokeh_chart(div)
+#     with col3:
+#         url = "https://medium.com/@siddh30"
+#         if st.button("Medium", use_container_width=True):
+#                 js = "window.open" + "('" + url + "')"  # New tab
+#                 html = '<img src onerror="{}">'.format(js)
+#                 div = Div(text=html)
+#                 st.bokeh_chart(div)
 
         
